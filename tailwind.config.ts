708c2baaeb33309bdf97853a9cd03eb7cd8761ea
wolfcodie <abdelkarim98elalaoui@gmail.com>
2024-09-29 01:04:20 +0100

    Add fonts, icons, images, and color scheme for UI enhancement

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        lota: ['Lota Grotesque Alt 3', 'serif'],
       },
+      colors: {
+        white: '#FFFFFF',
+        primary: '#7B6DFF',
+        secondary: '#FF6347',
+        black: '#19172C',
+        text: '#585665',
+      },
+      backgroundImage: (theme) => ({
+        'gradient-to-r': 'linear-gradient(to right, #6EE7B7, #3B82F6)',
+      }),
     },
   },
   plugins: [],
